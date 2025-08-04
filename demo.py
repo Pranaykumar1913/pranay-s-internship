e3bdf54e09914c6eb4f852c72cba880e807c21c5
Your Name <1913pranaykumar@gmail.com>
2025-08-04 17:27:37 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+df = pd.read_csv('C:\\Users\\Pranai Kumar\\OneDrive\\Documents\\car_prices.csv')
+#print(df.head())     # First 5 rows to get a feel
+#print(df.info())     # Data types and missing values info
+#print(df.describe()) # Summary statistics for numeric columns
+#print(df.isna().sum())  # Count missing values per column
+#df.drop_duplicates(inplace=True)
+#print (df)
+df['make'] = df['make'].str.lower().str.strip()
+#print(df['make'].unique())
+# Convert to datetime
+df['saledate'] = pd.to_datetime(df['saledate'], dayfirst=True, errors='coerce')
+print(df)
